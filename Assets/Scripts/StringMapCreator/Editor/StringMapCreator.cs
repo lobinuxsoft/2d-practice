bcbc0fd9f52b02539ddfee64b4de6b1d8fbd2774
Lobinux <lobinux@digilearnnials.com>
2022-05-30 02:07:14 -0300

    Improve String Map

--- Assets/Scripts/StringMapCreator/Editor/StringMapCreator.cs
+++ Assets/Scripts/StringMapCreator/Editor/StringMapCreator.cs
@@ -23,13 +23,7 @@ public class StringMapCreator : EditorWindow
         textAreaStyle = EditorGUIUtility.GetBuiltinSkin(EditorSkin.Scene).textArea;
         textAreaStyle.font = Resources.Load<Font>("StringMap");
 
-        maps = new List<TextAsset>();
-        var temp = Resources.LoadAll<TextAsset>("Maps");
-
-        foreach (var map in temp)
-        {
-            maps.Add(map);
-        }
+        LoadMapFromText();
 
         stringMap = FindObjectOfType<StringMap>();
 
@@ -37,11 +31,35 @@ public class StringMapCreator : EditorWindow
         {
             stringMap = new GameObject("String Map").AddComponent<StringMap>();
         }
+
+        stringMap.OnValidate();
+    }
+
+    private void LoadMapFromText()
+    {
+        if (maps == null) 
+        {
+            maps = new List<TextAsset>();
+        }
+        else
+        {
+            maps.Clear();
+            Resources.UnloadUnusedAssets();
+        }
+
+        var temp = Resources.LoadAll<TextAsset>("Maps");
+
+        foreach (var map in temp)
+        {
+            maps.Add(map);
+        }
+
+        if (maps.Count > 0) mapString = maps[maps.Count - 1].text;
     }
 
     private void OnGUI()
     {
-        rect = new Rect(0, 20, position.width, position.height);
+        rect = new Rect(10, 50, position.width-20, position.height - 60);
 
         mapString = GUI.TextArea(rect, mapString, Mathf.RoundToInt(rect.width * rect.height), textAreaStyle);
 
@@ -52,34 +70,56 @@ public class StringMapCreator : EditorWindow
 
     private void DrawMenuBar()
     {
-        Rect menuBar = new Rect(0, 0, position.width, 20);
+        Rect menuBar = new Rect(0, 0, position.width, 40);
         GUILayout.BeginArea(menuBar, EditorStyles.toolbar);
 
         GUILayout.BeginHorizontal();
 
         if(maps != null && maps.Count > 0)
         {
-            foreach (var map in maps)
+            for (int i = 0; i < maps.Count; i++)
             {
-                if (GUILayout.Toggle(mapString.Equals(map.text), new GUIContent(map.name), EditorStyles.toolbarButton))
+                if (GUILayout.Toggle(mapString.Equals(maps[i].text), new GUIContent(maps[i].name), EditorStyles.toolbarButton))
                 {
-                    mapString = map.text;
+                    mapString = maps[i].text;
+                    stringMap.Index = i;
                 }
             }
         }
 
-        if(GUILayout.Button("Save Map", EditorStyles.toolbarButton))
+        
+
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Save Map", EditorStyles.toolbarButton))
         {
             TextAsset map = new TextAsset(mapString);
             map.name = $"map{maps.Count}";
             var path = EditorUtility.SaveFilePanel("Save Map", Application.dataPath, map.name, "txt");
 
-            if (!string.IsNullOrEmpty(path)) 
+            if (!string.IsNullOrEmpty(path))
             {
-                maps.Add(map);
                 File.WriteAllText(path, map.text);
                 AssetDatabase.SaveAssets();
                 AssetDatabase.Refresh();
+            }
+
+            LoadMapFromText();
+            stringMap.OnValidate();
+        }
+
+        if (GUILayout.Button("Generate Map", EditorStyles.toolbarButton))
+        {
+            stringMap.GenerateMap();
+        }
+
+        if (stringMap.transform.childCount > 0)
+        {
+            if (GUILayout.Button("Delete Generated Map", EditorStyles.toolbarButton))
+            {
+                stringMap.DeleteMapObj();
             }
         }
 
--- Assets/Scripts/StringMapCreator/Runtime/StringMap.cs
+++ Assets/Scripts/StringMapCreator/Runtime/StringMap.cs
@@ -1,18 +1,80 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 public class StringMap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField, Min(0)] int index = 0;
+    [SerializeField] List<TextAsset> maps;
+    [SerializeField] List<CellData> cellsData;
+
+    public int Index
     {
-        
+        get => index;
+        set => index = value;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnValidate()
     {
-        
+        if(maps == null)
+            maps = new List<TextAsset>();
+        else
+        {
+            maps.Clear();
+            Resources.UnloadUnusedAssets();
+        }
+
+        var temp = Resources.LoadAll<TextAsset>("Maps");
+
+        foreach (var map in temp)
+        {
+            maps.Add(map);
+        }
     }
+
+    public void GenerateMap()
+    {
+        DeleteMapObj();
+
+        string[] lines = Regex.Split(maps[index].text, "\n");
+
+        for (int row = 0; row < lines.Length; row++)
+        {
+            for (int col = 0; col < lines[row].Length; col++)
+            {
+                if(lines[row][col] != ' ')
+                {
+                    var celldata = cellsData.Find((x)=>x.key.Equals(lines[row][col]));
+                
+
+                    if (celldata.prefab != null)
+                    {
+                        GameObject go = Instantiate(celldata.prefab, this.transform);
+                        go.transform.localPosition = new Vector3(col, 0f, -row);
+                    
+                        go.name = $"({row},{col})";
+                    }
+                    else
+                    {
+                        Debug.LogError($"No se encontro cell data en la key: {lines[row][col]}");
+                    }
+                }
+            }
+        }
+    }
+
+    public void DeleteMapObj()
+    {
+        while(transform.childCount > 0)
+        {
+            DestroyImmediate(transform.GetChild(0).gameObject);
+        }
+    }
+}
+
+[System.Serializable]
+public struct CellData
+{
+    public char key;
+    public GameObject prefab;
 }
